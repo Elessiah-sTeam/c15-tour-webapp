698f86fc7470321d4d08755ab7de17c11c4eb5f3
Elessiah <kgalaxie84@gmail.com>
2026-02-01 19:24:18 +0100

    Fix #14 : Nettoyage des erreurs

--- src/components/Panels/ConfigPanel.tsx
+++ src/components/Panels/ConfigPanel.tsx
@@ -1,6 +1,5 @@
 import './Panels.css';
 import Route from "./Route.tsx";
-import type {ItineraryModel} from "../../customObject/Itinerary/ItineraryModel.ts";
 import ItineraryTitle from "./ItineraryTitle.tsx";
 import ActionButtons from "./ActionButtons.tsx";
 
