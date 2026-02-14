3ea1481a1431f71f0c189a162402427f338cb91a
Elessiah <kgalaxie84@gmail.com>
2026-02-14 16:14:13 +0100

    Fix: Build

--- src/components/Panels/DragNDrop/SortableCategory.tsx
+++ src/components/Panels/DragNDrop/SortableCategory.tsx
@@ -47,6 +47,7 @@ export default function SortableCategory({ category, visible, idActiveItem}: Pro
                 title: "Nouveau segment",
                 hour: new Date(),
                 duration: new TimeSpan(),
+                distance: 0,
             },
             isStartEnd: false,
             steps: new Array<Step>()
