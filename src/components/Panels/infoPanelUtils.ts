3dfc544f92373dec379068d6558fe42a369d3639
Elessiah <kgalaxie84@gmail.com>
2026-06-15 14:13:43 +0200

    add pause line between segments

--- src/components/Panels/infoPanelUtils.ts
+++ src/components/Panels/infoPanelUtils.ts
@@ -1,5 +1,7 @@
 import { TimeSpan, TimespanOffset, type unitTimeSpan } from "../../customObject/TimeSpan.ts";
 
-export function formatTotalPauseDuration(totalPauseSeconds: number, units: unitTimeSpan): string {
+export const PAUSE_UNITS: unitTimeSpan = { days: "J ", hours: "H ", minutes: "MIN ", seconds: "S ", milliseconds: "MS " };
+
+export function formatTotalPauseDuration(totalPauseSeconds: number, units: unitTimeSpan = PAUSE_UNITS): string {
     return new TimeSpan(totalPauseSeconds * 1000).toFStr(TimespanOffset.MINUTES, units);
 }
